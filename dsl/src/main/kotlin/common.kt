826411d6c88369fa97f800220e163e34ed747978
y2k <itwisterlx@gmail.com>
2018-12-07 23:39:54 +0300

    prototype dsl

--- dsl/src/main/kotlin/common.kt
+++ dsl/src/main/kotlin/common.kt
@@ -1,4 +1,5 @@
 import com.google.gson.GsonBuilder
+import java.util.*
 
 fun printPretty(x: Any) {
     GsonBuilder()
@@ -6,4 +7,61 @@ fun printPretty(x: Any) {
         .create()
         .toJson(x)
         .let(::println)
+}
+
+class LinearLayout_ {
+    val `@class` = "LinearLayout"
+    var backgroundColor: Int = 0
+    var orientation: Int = 0
+    var padding: FloatArray = floatArrayOf()
+    val children = ArrayList<Any>()
+}
+
+class Button_ {
+    val `@class` = "Button"
+    var backgroundDrawableRes: Int = 0
+    var text: String = ""
+    var textColor: Int = 0
+    var textSize: Float = 0f
+    var onPressed: () -> Unit = {}
+    var padding: FloatArray = floatArrayOf()
+}
+
+class TextView_ {
+    val `@class` = "TextView"
+    var backgroundDrawableRes: Int = 0
+    var text: String = ""
+    var textColor: Int = 0
+    var textSize: Float = 0f
+    var padding: FloatArray = floatArrayOf()
+}
+
+private val stack = Stack<LinearLayout_>()
+
+fun linearLayout(f: LinearLayout_.() -> Unit): LinearLayout_ {
+    val l = LinearLayout_()
+    stack.push(l)
+    l.f()
+    stack.pop()
+    return l
+}
+
+fun button(f: Button_.() -> Unit) {
+    val b = Button_()
+    b.f()
+    stack.peek().children.add(b)
+}
+
+fun textView(f: TextView_.() -> Unit) {
+    val t = TextView_()
+    t.f()
+    stack.peek().children.add(t)
+}
+
+@Suppress("ClassName")
+object R {
+    object drawable {
+        const val button_bg: Int = 0
+        const val textview_bg: Int = 0
+    }
 }
--- dsl/src/main/kotlin/dsl-example.kt
+++ dsl/src/main/kotlin/dsl-example.kt
@@ -2,20 +2,16 @@
 
 import android.graphics.Color
 import android.widget.LinearLayout
-import java.util.*
 
 fun main(args: Array<String>) {
-    val x =
-        view(
-            "10",
-            { /* Increase */ },
-            { /* Decrease */ }
-        )
-
-    printPretty(x)
+    view(
+        "10",
+        { /* Increase */ },
+        { /* Decrease */ }
+    ).let(::printPretty)
 }
 
-private fun view(counter: String, onUp: () -> Unit, onDown: () -> Unit): Any =
+private fun view(counter: String, onUp: () -> Unit, onDown: () -> Unit) =
     linearLayout {
         backgroundColor = Color.WHITE
         orientation = LinearLayout.VERTICAL
@@ -29,80 +25,21 @@ private fun view(counter: String, onUp: () -> Unit, onDown: () -> Unit): Any =
         counterButton("-", onDown)
     }
 
-private fun counterButton(t: String, e: () -> Unit) {
+private fun counterButton(title: String, e: () -> Unit) =
     button {
         padding = floatArrayOf(4f)
         backgroundDrawableRes = R.drawable.button_bg
         textColor = Color.BLUE
         textSize = 18f
-        text = t
+        text = title
         onPressed = e
     }
-}
 
-private fun h1(t: String) {
+private fun h1(title: String) =
     textView {
         padding = floatArrayOf(4f)
         backgroundDrawableRes = R.drawable.textview_bg
         textColor = Color.GRAY
         textSize = 20f
-        text = t
+        text = title
     }
-}
-
-class LinearLayout_ {
-    val `@class` = "LinearLayout"
-    var backgroundColor: Int = 0
-    var orientation: Int = 0
-    var padding: FloatArray = floatArrayOf()
-    val children = ArrayList<Any>()
-}
-
-class Button_ {
-    val `@class` = "Button"
-    var backgroundDrawableRes: Int = 0
-    var text: String = ""
-    var textColor: Int = 0
-    var textSize: Float = 0f
-    var onPressed: () -> Unit = {}
-    var padding: FloatArray = floatArrayOf()
-}
-
-class TextView_ {
-    val `@class` = "TextView"
-    var backgroundDrawableRes: Int = 0
-    var text: String = ""
-    var textColor: Int = 0
-    var textSize: Float = 0f
-    var padding: FloatArray = floatArrayOf()
-}
-
-private val stack = Stack<LinearLayout_>()
-
-fun linearLayout(f: LinearLayout_.() -> Unit): LinearLayout_ {
-    val l = LinearLayout_()
-    stack.push(l)
-    l.f()
-    stack.pop()
-    return l
-}
-
-fun button(f: Button_.() -> Unit) {
-    val b = Button_()
-    b.f()
-    stack.peek().children.add(b)
-}
-
-fun textView(f: TextView_.() -> Unit) {
-    val t = TextView_()
-    t.f()
-    stack.peek().children.add(t)
-}
-
-@Suppress("ClassName")
-object R {
-    object drawable {
-        const val button_bg: Int = 0
-        const val textview_bg: Int = 0
-    }
-}
