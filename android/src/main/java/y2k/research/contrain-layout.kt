c1728517d37a5f2470df342437effa3fb7324322
y2k <itwisterlx@gmail.com>
2018-12-23 14:06:58 +0300

    layout-flatting research

---
+++ android/src/main/java/y2k/research/contrain-layout.kt
@@ -0,0 +1,7 @@
+package y2k.research
+
+import android.support.constraint.ConstraintLayout
+
+interface ConstraintLayoutF<T>
+
+class RealConstraintLayoutF : ConstraintLayoutF<ConstraintLayout>
--- dsl/src/main/kotlin/constraintlayout-flatting-example.kt
+++ dsl/src/main/kotlin/constraintlayout-flatting-example.kt
@@ -1,40 +1,92 @@
-import android.content.Context
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.TextView
+import java.util.*
 
-fun main(args: Array<String>) = Unit
+fun main(args: Array<String>) {
+    TODO()
+}
 
-fun foo(context: Context) {
-    VBox(context).apply {
-        addView(
-            TextView(context).apply {
-                text = "Login page"
-            }
-        )
-        addView(
-            HBox(context).apply {
-                setPadding(8, 8, 8, 8)
+private var globalId = 1
+private val decoratorStack = Stack<Group>()
+private val prevViewsInGroup = Stack<TView>()
 
-                addView(
-                    Button(context).apply {
-                        text = "OK"
-                    }
-                )
-                addView(
-                    Button(context).apply {
-                        text = "Cancel"
-                    }
-                )
-            }
-        )
+private fun <Child, T, P> test(child: Child, constrainLayout: T, F: ConstraintLayoutF<T, P>) {
+    when (child) {
+        is VBox -> {
+            decoratorStack.push(child)
+            prevViewsInGroup.clear()
+        }
+        is HBox -> {
+            decoratorStack.push(child)
+            prevViewsInGroup.clear()
+        }
+        is Box -> {
+            decoratorStack.push(child)
+            prevViewsInGroup.clear()
+        }
+        is PaddingBox -> {
+            prevViewsInGroup.clear()
+            decoratorStack.push(child)
+            prevViewsInGroup.clear()
+        }
+        is TView -> {
+            F.addView(constrainLayout, child, makeParams(F))
+            prevViewsInGroup.add(child)
+        }
+        else -> error(child.toString())
     }
 }
 
-class VBox(context: Context?) : FrameLayout(context)
+fun <T, P> makeParams(F: ConstraintLayoutF<T, P>): P {
 
-class HBox(context: Context?) : FrameLayout(context)
 
-class Box(context: Context?) : FrameLayout(context)
+    TODO()
+}
 
-class PaddingBox(context: Context?) : FrameLayout(context)
+private fun mkExample(): Group =
+    VBox(
+        TTextView().apply {
+            text = "Login page"
+        },
+        PaddingBox(
+            8,
+            HBox(
+                TButton().apply {
+                    text = "OK"
+                },
+                TButton().apply {
+                    text = "Cancel"
+                }
+            )
+        )
+    )
+
+open class TView
+
+open class TTextView : TView() {
+    var text: String = ""
+}
+
+class TButton : TTextView()
+
+typealias Child = Any
+
+open class Group : Child()
+
+class VBox(vararg children: Child) : Group()
+
+class HBox(vararg children: Child) : Group()
+
+class Box(vararg children: Child) : Group()
+
+class PaddingBox(val padding: Int, val child: Child) : Group()
+
+interface ConstraintLayoutF<T, P> {
+    fun addView(constrainLayout: T, child: Any, params: P)
+}
+
+object ConstraintLayoutImpl : ConstraintLayoutF<FakeConstraintLayout, FakeConstraintLayoutParams> {
+    override fun addView(constrainLayout: FakeConstraintLayout, child: Any, params: FakeConstraintLayoutParams): Unit =
+        TODO()
+}
+
+class FakeConstraintLayout
+class FakeConstraintLayoutParams
