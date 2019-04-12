35156cf98bc89c0a3e484de4f33972a3e095f920
y2k <itwisterlx@gmail.com>
2019-04-12 13:41:27 +0300

    refactor dsl example

---
+++ android/src/main/java/y2k/android/DslExampleActivity.kt
@@ -0,0 +1,104 @@
+package y2k.android
+
+import android.R
+import android.app.Activity
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import y2k.android.common.getDrawableByAttr
+import y2k.virtual.ui.*
+
+class DslExampleActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val root = LinearLayout(this)
+        setContentView(root)
+
+        var s1: VirtualNode? = null
+        var state = 0
+        fun update() {
+            val s2 = mkNode { makeStage(state++) { update() } }
+            updateRealView(root, s1, s2)
+            s1 = s2
+        }
+        update()
+    }
+
+    private fun makeStage(stage: Int, dispatch: () -> Unit) =
+        linearLayout {
+            backgroundColor = color4
+            gravity = Gravity.CENTER_VERTICAL
+            orientation = LinearLayout.VERTICAL
+
+            textView {
+                text = "Line #1"
+                textColor = color3
+                textSize = 20f
+            }
+
+            if (stage >= 1)
+                textView {
+                    text = "Line #2"
+                    textColor = color3
+                    textSize = 16f
+                }
+
+            textView {
+                text = "Line #3"
+                textColor = color2
+                textSize = 12f
+            }
+
+            if (stage >= 2)
+                imageView {
+                    scaleType = ImageView.ScaleType.FIT_CENTER
+                    imageDrawable = getDrawable(R.drawable.ic_menu_view)!!
+                }
+
+            if (stage >= 3)
+                textView {
+                    text = "Line #4"
+                    textColor = color3
+                    textSize = 16f
+                }
+
+            linearLayout {
+                button {
+                    background = getDrawableByAttr(R.attr.selectableItemBackground)
+                    text = "Line #4 ($stage)"
+                    textColor = color1
+                    textSize = 14f
+                    onClickListener = View.OnClickListener { dispatch() }
+                }
+
+                customButton(stage >= 1)
+            }
+        }
+
+    private fun customButton(showProgress: Boolean) =
+        frameLayout {
+            if (!showProgress)
+                button {
+                    background = getDrawableByAttr(R.attr.selectableItemBackground)
+                    text = "Line #5"
+                    textColor = color1
+                    textSize = 14f
+                }
+
+            if (showProgress)
+                progressBar {
+                    elevation = 10f
+                }
+        }
+
+    companion object {
+        const val color1 = 0xFF03a5f3.toInt()
+        const val color2 = 0xFF98A098.toInt()
+        const val color3 = 0xFFFFFFFF.toInt()
+        const val color4 = 0xFF233544.toInt()
+    }
+}
--- android/src/main/java/y2k/android/common/common.kt
+++ android/src/main/java/y2k/android/common/common.kt
@@ -1,8 +1,17 @@
 package y2k.android.common
 
+import android.content.Context
+import android.graphics.drawable.Drawable
 import android.os.Handler
 import android.os.Looper
 
 fun timeout(delay: Long, f: () -> Unit) {
     Handler(Looper.getMainLooper()).postDelayed(f, delay)
+}
+
+fun Context.getDrawableByAttr(selectableItemBackground: Int): Drawable {
+    val ta = obtainStyledAttributes(intArrayOf(selectableItemBackground))
+    val drawableFromTheme = ta.getDrawable(0)
+    ta.recycle()
+    return drawableFromTheme
 }
